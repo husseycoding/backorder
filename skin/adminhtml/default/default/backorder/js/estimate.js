6049c3bd82b5be4a7e5236c7a78e1e78d41fa591
Hussey Coding <hussey@husseycoding.co.uk>
2015-10-08 10:34:44 +0100

    added fixed display of delivery estimates for orders - fixes #10

--- skin/adminhtml/default/default/backorder/js/estimate.js
+++ skin/adminhtml/default/default/backorder/js/estimate.js
@@ -1,7 +1,9 @@
 var backorder = Class.create({
     afterInit: function() {
         $H(this.orderestimates).each(function(estimate) {
-            $("order_item_" + estimate.key + "_title").insert({after: "<p class=\"dispatch-estimate\">" + this.estimatetext + ": <span>" + estimate.value + "</span></p>"});
+            if (estimate.value) {
+                $("order_item_" + estimate.key + "_title").insert({after: "<p class=\"dispatch-estimate\">" + this.estimatetext + ": <span>" + estimate.value + "</span></p>"});
+            }
         }.bind(this));
     }
 });
--- skin/frontend/base/default/js/backorder/customer.js
+++ skin/frontend/base/default/js/backorder/customer.js
@@ -1,7 +1,9 @@
 var backorder = Class.create({
     afterInit: function() {
         $H(this.orderestimates).each(function(estimate) {
-            $("order-item-row-" + estimate.key).down("h3").insert({after: "<p class=\"dispatch-estimate\">" + this.estimatetext + ": <span>" + estimate.value + "</span></p>"});
+            if (estimate.value) {
+                $("order-item-row-" + estimate.key).down("h3").insert({after: "<p class=\"dispatch-estimate\">" + this.estimatetext + ": <span>" + estimate.value + "</span></p>"});
+            }
         }.bind(this));
     }
 });
